e298e1a78d92ded2f408182108edb89a423c11dd
PistonShot1 <nandaprian7@gmail.com>
2022-05-12 09:02:38 +0800

    updated

---
+++ testing.js
@@ -0,0 +1,11 @@
+
+document.getElementById("button1").onclick = function (){
+    location.replace('https://docs.google.com/document/d/1H0NzIxPzSCXQ06PTIWouwRCWCpxPPe4A3ztPsSS3zfI/edit?usp=sharing')
+}
+document.getElementById("button2").onclick = function (){
+    location.replace('https://www.canva.com/design/DAE-as7rDyk/nerXU4DBNmgs397mzm_7Xw/edit?utm_content=DAE-as7rDyk&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton')
+}
+document.getElementById("button3").onclick = function (){
+    location.replace('https://drive.google.com/file/d/186cAbhrM2M2OOHaOchGzIcgReqEjbMex/view?usp=sharing')
+
+}
