bcf204cf2cc982baf00e3c8efa06f3710a68b8ba
PistonShot1 <nandaprian7@gmail.com>
2022-05-19 00:33:52 +0800

    updated

--- testing.js
+++ testing.js
@@ -2,10 +2,9 @@
 document.getElementById("button1").onclick = function (){
     location.replace('https://docs.google.com/document/d/1H0NzIxPzSCXQ06PTIWouwRCWCpxPPe4A3ztPsSS3zfI/edit?usp=sharing')
 }
-document.getElementById("button2").onclick = function (){
-    location.replace('https://www.canva.com/design/DAE-as7rDyk/nerXU4DBNmgs397mzm_7Xw/edit?utm_content=DAE-as7rDyk&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton')
-}
+document.getElementById("button2").onclick = () => location.replace('https://www.canva.com/design/DAE-as7rDyk/nerXU4DBNmgs397mzm_7Xw/edit?utm_content=DAE-as7rDyk&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton') ;
 document.getElementById("button3").onclick = function (){
     location.replace('https://drive.google.com/file/d/186cAbhrM2M2OOHaOchGzIcgReqEjbMex/view?usp=sharing')
-
+    
 }
+document.getElementById("button4").onclick = () => location.replace('https://docs.google.com/document/d/1AGg9v7yhwdvaQD7qLO2RG4ypYtjVtRnUs_FYmqlI014/edit') ;
